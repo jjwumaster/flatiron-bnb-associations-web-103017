eef12910208be9ab6fd2a86f7964c275ed7708cd
Jake <jakemacnaughton@gmail.com>
2017-11-13 18:08:06 -0500

    Done

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User < ActiveRecord::Base
   has_many :listings, :foreign_key => 'host_id'
   has_many :reviews, :foreign_key => 'guest_id'
   has_many :reservations, through: :listings
-  has_many :reservations, :foreign_key => 'guest_id'
+  has_many :trips, class_name: "Reservation", :foreign_key => 'guest_id'
 
-  def trips
-    binding.pry
-    self.reservations
-  end
+  # def trips
+  #   self.reservations
+  # end
 end
